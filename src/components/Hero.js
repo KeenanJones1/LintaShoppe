80dfed4f4da34324549e110d76c8a9b3137fa85f
Keenan Jones <keenanjones210@gmail.com>
2020-11-10 12:25:58 -0600

    removing lorem lipsum

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,9 @@
 import React, { useState }  from 'react'
+import { Link } from 'react-router-dom'
 import {Carousel} from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
 
 
  function Hero(){
@@ -14,24 +18,84 @@ import {Carousel} from 'react-bootstrap'
   <Carousel.Item>
     <svg className="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"/></svg>
     <Carousel.Caption>
-      <h3>FeaturedProduct</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      <div className="container">
+
+        <div className="row justify-content-md-center align-items-start">
+          <div>
+            <h3>Linta Shoppe</h3>
+          </div>
+
+        </div>
+        <div className="row justify-content-md-center ">
+          <h5 className="text-center">All Collections</h5>
+        </div>
+        <div className="row justify-content-md-center">
+          <div className="py-6">
+          <h5 className="text-center">Fast USA Shipping</h5>
+          </div>
+        </div>
+        <div className="row justify-content-md-center">
+          <div>
+          <h5 className="text-center">Free Shipping Worldwide</h5>
+          </div>
+        </div>
+        <div className="row justify-content-md-center">
+          <div>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </div>
+        </div>
+      </div>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
   <svg className="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"/></svg>
     <Carousel.Caption>
-      <h3>AboutUs</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h3>About us</h3>
+        <p>
+          LintaShoppe.com was created out of a vision that has been developing for over 3 years. It founders with collectively over 3 years of accessory experience dreamed of a place where customers could find the greatest selection and lowest priced accessories for their daily life. We want to help you find all the essential accessories you need.
+
+          {/* Our professional customer service team is always here to help you. Your satisfaction is our top priority!
+          Moreover, you can easily get your order anywhere in the world to our worldwide Free shipping service!
+
+          Let us show you why so many customers are truly satisfied with our customer service and quality of products. We are looking forward to serving you to the best of our abilities and hope you become a returning customer.
+
+          Enjoy your shopping at LintaShoppe.com */}
+          </p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
     <svg className="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"/></svg>
     <Carousel.Caption>
-      <h3>ContactUs</h3>
+      <h3>Contact Us</h3>
       <p>
-      Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+      For any questions on products, please feel free to email us at support@lintashoppe.com
+       We will get back to you within 24 hours!
       </p>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm">
+            <Link>
+              <span className="mr-2">
+                Call us
+              </span>
+              <span>
+                <FontAwesomeIcon icon={faPhone}/>
+              </span>
+            </Link>
+          </div>
+
+          <div className="col-sm">
+            <Link>
+              <span className="mr-2">
+                Email us
+              </span>
+              <span>
+                <FontAwesomeIcon icon={faEnvelope}/>
+              </span>
+            </Link>
+          </div>
+        </div>
+      </div>
     </Carousel.Caption>
   </Carousel.Item>
 </Carousel>
@@ -39,3 +103,24 @@ import {Carousel} from 'react-bootstrap'
 }
 
 export default Hero;
+
+
+     
+
+
+{/* <div className="col-4">
+          <h5 className="text-center">All Collections</h5>
+          </div>
+
+          <div className="col-4">
+            
+          </div>
+
+          <div className="col-4">
+            
+          </div>
+
+
+          <div className="col-4">
+            
+          </div> */}
--- src/hocs/Navbar.js
+++ src/hocs/Navbar.js
@@ -9,8 +9,12 @@ function Navbar() {
    <header>
     <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
       <Link className="navbar-brand" to="/">Linta Shoppe</Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+        <button className="navbar-toggler" type="button" aria-expanded="false">
+        <Link className="nav-link" to='/cart'>
+              <svg width="2em" height="2em" viewBox="0 0 16 16" className="bi bi-basket" fill="white" xmlns="http://www.w3.org/2000/svg">
+                <path fillRule="evenodd" d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1v4.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 13.5V9a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h1.217L5.07 1.243a.5.5 0 0 1 .686-.172zM2 9v4.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V9H2zM1 7v1h14V7H1zm3 3a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 4 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 6 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 8 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5z"/>
+              </svg>
+            </Link>
         </button>
 
     
@@ -28,17 +32,7 @@ function Navbar() {
 
         
 
-          {/* Search Button start */}
-        <form>
-          <input type="search" name="" id=""/>
-          <button>
-            <svg width="2em" height="2em" viewBox="0 0 16 16" className="bi bi-search" fill="white" xmlns="http://www.w3.org/2000/svg">
-                <path fillRule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
-                <path fillRule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
-              </svg>
-          </button>
-        </form>
-      {/* Search button Ends  */}
+
 
         <div>
             <Link className="nav-link" to='/cart'>
